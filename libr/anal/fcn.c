665a492f2e614ac0ccdc6e6401d748d4479108ab
pancake <pancake@nopcode.org>
2018-11-21 22:43:00 +0100

    Use r_return_.. in r_anal_op and reduce some code paths in xrefs search ##anal

--- libr/anal/fcn.c
+++ libr/anal/fcn.c
@@ -959,10 +959,15 @@ repeat:
 			VERBOSE_ANAL eprintf ("FFFF opcode at 0x%08"PFMT64x "\n", addr + idx);
 			return R_ANAL_RET_ERROR;
 		}
-		if ((oplen = r_anal_op (anal, &op, addr + idx, buf + addrbytes * idx, len - addrbytes * idx, R_ANAL_OP_MASK_ALL)) < 1) {
+		if ((oplen = r_anal_op (anal, &op, addr + idx, buf + (addrbytes * idx), len - (addrbytes * idx), R_ANAL_OP_MASK_ALL)) < 1) {
 			RCore *core = anal->coreb.core;
 			if (!core || !core->bin || !core->bin->is_debugger) { // HACK
-				eprintf ("Invalid instruction (possibly truncated) at 0x%"PFMT64x"\n", addr + idx);
+				ut8 *b = buf  + (addrbytes  * idx);
+				eprintf ("BUTES bits %d %02x %02x %02x %02x\n",
+						anal->bits, b[0], b[1], b[2], b[3]);
+
+				eprintf ("Invalid instruction in %d (possibly truncated) at 0x%"PFMT64x"\n", len - (addrbytes * idx), addr + idx);
+			return R_ANAL_RET_ERROR;
 			}
 			gotoBeach (R_ANAL_RET_END);
 		}
